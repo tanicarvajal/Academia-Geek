6a435b58cc0dfad1b75e91d04a763b89b2cf5ac4
Tania Carvajal <carvajaltaniav@gmail.com>
2021-05-13 10:50:20 -0500

    clase6

---
+++ clase8/scripts/script.js
@@ -0,0 +1,48 @@
+/* Estructura switch 
+ switch (expresion)
+ */
+/* Ejercicio uno */
+//  let dia = 4;
+
+//  switch(dia){
+//      case 0:
+//         console.log('Es domingo');
+//         break;
+//      case 1:
+//         console.log('Es lunes');
+//         break;
+//      case 2:
+//         console.log('Es martes');
+//         break;
+//      case 3:
+//         console.log('Es miercoles');
+//         break;
+//      case 4:
+//         console.log('Es jueves');
+//         break;
+//      case 5:
+//         console.log('Es viernes');
+//         break;
+//      case 6:
+//         console.log('Es sabado');
+//         break;
+//      default:
+//          console.log('El dia ejecutado no existe');
+//          break;
+//  } 
+
+ let expr = '';
+
+ switch(expr){
+    case 'Naranjas':
+        console.log('El kilo de naranjas cuesta 10000');
+         break;
+    case 'Manzanas':
+        console.log('El kilo de manzanas cuesta 10000');
+        break;
+    case 'Platanos':
+    case 'Mangos':
+        console.log('El kilo de platanos y mangos cuesta 10000');
+        break;
+ }
+
